f97752a0548967e707fd637dd8a9151efe577ce2
austintip <austin.d.tipograph@gmail.com>
2021-01-24 15:57:41 -0500

    api key header that didnt break anything

--- routes/info.js
+++ routes/info.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const API_KEY= process.env.API_KEY
 
 router.get('/:id', (req, res) => {
     let dogBreed = req.params.id;
-    let dogUrl = `https://api.TheDogAPI.com/v1/images/search?breed_ids=${dogBreed}`
-    axios.get(dogUrl).then(apiResponse => {
+    let dogUrl = `https://api.TheDogAPI.com/v1/images/search?breed_id=${dogBreed}`
+    axios.get(dogUrl, { 
+        headers: {
+            'x-api-key': `${API_KEY}`
+    }})
+    .then(apiResponse => {
         let dogs = apiResponse.data;
         console.log(dogs);
         console.log('🥶')
         console.log(dogs[0].url);
         res.render('dogs/info',{ dogs });
+    }).catch(err => {
+        console.log(err)
     });
 });
 
